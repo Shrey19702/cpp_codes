02ed11ced6e369870d57e3e706dc365b5646d6f9
Shrey19702 <90191696+Shrey19702@users.noreply.github.com>
2021-09-25 02:39:49 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//......Prime No Test root(n).............
+bool prime(int x){ 
+    if(x%2==0){
+        return false;
+    }
+    for(int i=3; i*i <=x; i+=2){
+        if(x%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+//......no. of primes before n  O( n*log(log(n)) .............
+int countPrimes(int n) {
+    if(n<3){
+        return 0;
+    }
+
+    bool arr[n];
+    for(int i=0; i<n; i++){
+        arr[i]=true;
+    }
+
+    for(int p=2; p*p<=n; p++){
+        for(int i=p*p; i<n; i+=p){
+            arr[i]=false;
+        }
+    }
+    int count=0;
+    for(int i=2; i<n; i++){
+        if(arr[i]){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+
 int main(){
   
   return 0;
